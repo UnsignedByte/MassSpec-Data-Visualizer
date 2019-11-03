61182438161054842d9231a38e0d8e60f0431c18
UnsignedByte <edlam1889@gmail.com>
2019-11-03 00:09:35 -0700

    finish v12

--- HeatMap/FileCat_v12.m
+++ HeatMap/FileCat_v12.m
@@ -87,39 +87,40 @@ end
 
 ProteinNames = sortrows(ProteinNames,2); %sort names by id
 
-tic;
+toc;
 
 UniqueColumns = [7]; %Columns to take from each dataset (sorted by max and sum of first elem)
 %Will save max and sum as well as values from the datasets
 SingleColumns = [11]; %Columns equal across all datasets (will only take one column)
 
-Result = NaN(length(ProteinNames),(NumFilesRead+2)*length(UniqueColumns)+SingleColumns);
+Result = NaN(length(ProteinNames),(NumFilesRead+2)*length(UniqueColumns)+length(SingleColumns));
 
 i = 1;
 j = 1;
-while i < ProteinNames{end,2}
-    while ProteinNames{j,2} == i
+while i <= ProteinNames{end,2}
+    while j <= size(ProteinNames, 1) && ProteinNames{j,2} == i
         for kk = 1:NumFilesRead
             ind = find(ismember(TempStruct(kk).dat.Description, ProteinNames{j,1})); %find index of peptide
             if ~isempty(ind)
                 for k = 1:length(UniqueColumns)
-                    Result(j,k*5-5+kk) = table2array(TempStruct(kk).dat(ind,UniqueColumns(k))); %paste data into result
+                    Result(j,(k-1)*(NumFilesRead+2)+kk) = table2array(TempStruct(kk).dat(ind,UniqueColumns(k))); %paste data into result
                 end
             
                 %unique columns should be equal across all datasets, so only
                 %run once
                 if kk == 1
                     for k = 1:length(SingleColumns)
-                        Result(j,length(UniqueColumns)*5+k) = table2array(TempStruct(1).dat(ind,UniqueColumns(k))); %paste data into result
+                        Result(j,length(UniqueColumns)*(NumFilesRead+2)+k) = table2array(TempStruct(1).dat(ind,UniqueColumns(k))); %paste data into result
                     end
                 end
             end
         end
         % add sum and max columns
         for k = 1:length(UniqueColumns)
-            if ~all(isnan(Result(j,k*5-4:k*5-2))) %check if all elems are NaN, if so, skip
-                Result(j, k*5-1)=max(Result(j,k*5-4:k*5-2));
-                Result(j, k*5)=nansum(Result(j,k*5-4:k*5-2));
+            slice = Result(j,(k-1)*(NumFilesRead+2)+1:k*(NumFilesRead+2)-2);
+            if ~all(isnan(slice)) %check if all elems are NaN, if so, skip
+                Result(j, k*(NumFilesRead+2)-1)=max(slice);
+                Result(j, k*(NumFilesRead+2))=nansum(slice);
             end
         end
         j = j + 1;
@@ -127,53 +128,29 @@ while i < ProteinNames{end,2}
     i = i + 1;
 end
 
-%There is a string comparison loop that starts at the top of the unique
-%list, and goes through each protein name list. If it finds it, it
-%transfers the number of 'counts' into the appropriate column. otherwise,
-%it leaves in a NaN
-
-
-FileOutTemp = cell(length(FileOutUni),NumFilesRead);
-FileOutTemp(:) = {NaN}; 
-FileOutAAs = cell(length(FileOutUni),1);
-FileOutCombined = [FileOutUni, FileOutTemp];
-toc;
-
-for k = 1 : NumFilesRead
-    for i = 1:length(FileOutCombined(:,1))
-        for j = 1:length(TempStruct(k).CountsUsed(:,1))
-            if strcmp(FileOutCombined(i,1),TempStruct(k).CountsUsed(j,1)) == 1
-                FileOutCombined(i,k+1) = TempStruct(k).CountsUsed(j,2);
-                FileOutAAs(i,1) = TempStruct(k).AAsUsed(j);
-            end;
-        end;
-    end;
-    toc;
-end;
-
 %New little bit of code to find the reverses and contaminants and flag them
 %for removal.
 toc;
-ReverseFinder = zeros(length(FileOutCombined(:,1)),1);
+ReverseFinder = zeros(size(Result,1),1);
 
 %Disabled crapome file, may remove entirely in the next version.
 % [num,craptxt,raw] = xlsread(crapome.file);
 %this file txt should be looped for each target
 
-for i = 1:length(FileOutCombined(:,1))
-    if isempty(strfind(FileOutCombined{i,1},'>Reverse')) == 0
+for i = 1:size(Result,1)
+    if ~isempty(strfind(ProteinNames{i,1},'>Reverse'))
         ReverseFinder(i,1) = 1;
-    elseif isempty(strfind(FileOutCombined{i,1},'Common contaminant')) == 0
+    elseif ~isempty(strfind(ProteinNames{i,1},'Common contaminant'))
         ReverseFinder(i,1) = 1;
-    elseif isempty(strfind(FileOutCombined{i,1},'eratin, type')) == 0
+    elseif ~isempty(strfind(ProteinNames{i,1},'eratin, type'))
         ReverseFinder(i,1) = 1;
-    elseif isempty(strfind(FileOutCombined{i,1},' desmo')) == 0
+    elseif ~isempty(strfind(ProteinNames{i,1},' desmo'))
         ReverseFinder(i,1) = 1;
-    elseif isempty(strfind(FileOutCombined{i,1},'dermi')) == 0
+    elseif ~isempty(strfind(ProteinNames{i,1},'dermi'))
         ReverseFinder(i,1) = 1;
-    elseif isempty(strfind(FileOutCombined{i,1},'plak')) == 0
+    elseif ~isempty(strfind(ProteinNames{i,1},'plak'))
         ReverseFinder(i,1) = 1;
-    end;
+    end
 % If this search is for the Jackson Lab, use the following segment:
 
 %     for j = 1:length(craptxt(:,1))
@@ -181,53 +158,57 @@ for i = 1:length(FileOutCombined(:,1))
 %             reversefinder(i,1) = 1;
 %         end;
 %     end;
-end;
+end
 
 ReverseFinder = num2cell(ReverseFinder);
 
-%Add in the Max, Sum, and Rank columns here
+Result(isnan(Result))=-Inf;
 
-MaxFinder = zeros(length(FileOutCombined(:,1)),1);
-SumFinder = zeros(length(FileOutCombined(:,1)),1);
+Result = num2cell(Result);
 
-for i = 1 : length(FileOutCombined(:,1))
-    MaxFinder(i,1) = max(cell2mat(FileOutCombined(i,2:NumFilesRead+1)),[],'omitnan');
-    SumFinder(i,1) = sum(cell2mat(FileOutCombined(i,2:NumFilesRead+1)),[],'omitnan');
-end;
-
-MaxFinder = num2cell(MaxFinder);
-SumFinder = num2cell(SumFinder);
-RankFinder = num2cell(zeros(length(FileOutCombined(:,1)),1));
-
-FileOutFinal = [RankFinder, FileOutCombined, ReverseFinder, MaxFinder, SumFinder, FileOutAAs];
+FileOutFinal = [ProteinNames(:, [2 1]), Result, ReverseFinder];
 toc;
 
 FileOutFinalDeComma = FileOutFinal;
 
-for i = 1:length(FileOutFinal(:,1))
-    if isempty(strfind(FileOutFinal{i,1},',')) == 0
-        FileOutFinalDeComma{i,1} = strrep(FileOutFinal{i,1}, ',', '-');
-    end;
-end;
+for i = 1:size(FileOutFinal, 1)
+    if isempty(strfind(FileOutFinal{i,2},',')) == 0
+        FileOutFinalDeComma{i,2} = strrep(FileOutFinal{i,2}, ',', '-');
+    end
+end
 
 %Here, I add in the file names in a readable format so they can be column
 %headers. Note that these column headers are a tad different than before so
 %they are readable, and should be based on the input file names as much as
 %possible.
 
-HeaderFileString = {'Rank_Number'; 'Protein_Name'};
+HeaderFileString = cell(3+size(Result,2), 1);
+HeaderFileString(1:2) = {'Rank_Number'; 'Protein_Name'};
+HeaderFileString{end} = 'Contaminant';
 
 for i = 1 : NumFilesRead
-    CleanedTempString = strrep(TempStruct(i).NameForFile, '.raw_20', '_');
-    CleanedTempString = strrep(CleanedTempString, '_Byonic','');
-    CleanedTempString = strrep(CleanedTempString, '_Elite','');
-    CleanedTempString = strrep(CleanedTempString, '_control',''); %Remove after I'm done with Janos' dataset
-    CleanedTempString = strrep(CleanedTempString, '25fmol_ul_6x5spike_',''); %Remove after I'm done with Janos' dataset
-    CleanedTempString = strcat('Out', CleanedTempString);
-    HeaderFileString = [HeaderFileString; CleanedTempString];
-end;
+    
+    CleanedFileName = strrep(TempStruct(i).NameForFile, '.raw_20', '_');
+    CleanedFileName = strrep(CleanedFileName, '_Byonic','');
+    CleanedFileName = strrep(CleanedFileName, '_Elite','');
+    CleanedFileName = strrep(CleanedFileName, '_control',''); %Remove after I'm done with Janos' dataset
+    CleanedFileName = strrep(CleanedFileName, '25fmol_ul_6x5spike_',''); %Remove after I'm done with Janos' dataset
+    %label by dataset
+    for j = 1:length(UniqueColumns)
+        HeaderFileString{2+(j-1)*(NumFilesRead+2)+i} = [TempStruct(1).dat.Properties.VariableNames{UniqueColumns(j)} '_' CleanedFileName];
+    end
+end
 
-HeaderFileString = [HeaderFileString; 'Contaminant'; 'Max'; 'Sum'; 'AAs'];
+%label single columns
+for j = 1:length(SingleColumns)
+    HeaderFileString{2+length(UniqueColumns)*(NumFilesRead+2)+j} = TempStruct(1).dat.Properties.VariableNames{SingleColumns(j)};
+end
+
+%label max and sum
+for j = 1:length(UniqueColumns)
+    HeaderFileString{2+j*(NumFilesRead+2)-1} = ['Max_' TempStruct(1).dat.Properties.VariableNames{UniqueColumns(j)}];
+    HeaderFileString{2+j*(NumFilesRead+2)} = ['Sum_' TempStruct(1).dat.Properties.VariableNames{UniqueColumns(j)}];
+end
 
 %Now we make the final output file for printing to a csv. we first make a
 %table rearrange it appropriately, and then have it fill in the rank number
@@ -235,9 +216,43 @@ HeaderFileString = [HeaderFileString; 'Contaminant'; 'Max'; 'Sum'; 'AAs'];
 
 FinalFileOut = cell2table(FileOutFinalDeComma);
 FinalFileOut.Properties.VariableNames = HeaderFileString;
-FinalFileOut = sortrows(FinalFileOut,{'Contaminant','Max','Sum'},{'ascend','descend','descend'});
 
-for i = 1:length(FileOutFinalDeComma)
-    FinalFileOut.Rank_Number(i) = i;
-end;
-writetable(FinalFileOut,'tableformat.csv');
+%Sort data by max, sum of first column by grouping
+UniqueIDS = unique([ProteinNames{:,2}]);
+maxes = zeros(length(UniqueIDS), 3);
+maxes(:,1) = UniqueIDS';
+
+i = 1;
+j = 1;
+while i <= size(maxes,1)
+    cMax = [NaN NaN];
+    while j <= size(ProteinNames, 1) && ProteinNames{j,2} == maxes(i,1)
+        cMax = max(cMax, table2array(FinalFileOut(j,3+NumFilesRead+(0:1))));
+        j = j + 1;
+    end
+    maxes(i,2:end) = cMax;
+    i = i + 1;
+end
+maxes = sortrows(maxes, [2 3], {'descend', 'descend'});
+
+for i = 1:size(maxes,1)
+    FinalFileOut.Rank_Number(FinalFileOut.Rank_Number==maxes(i,1), 1) = i;
+end
+
+FirstName = TempStruct(1).dat.Properties.VariableNames{UniqueColumns(1)}; %name of first column
+tempArr = table2array(FinalFileOut(:,3+NumFilesRead+(0:1)));
+FinalFileOut = sortrows(FinalFileOut,{'Contaminant', 'Rank_Number', ['Max_' FirstName],['Sum_' FirstName]},{'ascend','ascend','descend','descend'});
+
+currI = 0;
+currR = 0;
+
+for i = 1:size(FileOutFinalDeComma, 1)
+    if currI ~= FinalFileOut.Rank_Number(i)
+        currI = FinalFileOut.Rank_Number(i);
+        currR = currR + 1;
+    end
+    FinalFileOut.Rank_Number(i) = currR; %update rank
+end
+filename = 'tableformat.csv';
+writetable(FinalFileOut,filename)
+toc;
