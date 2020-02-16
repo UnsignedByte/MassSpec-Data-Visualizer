19162d7cbae712b3966d7b2122242749aaa5b24b
UnsignedByte <edlam1889@gmail.com>
2020-02-15 16:14:44 -0800

    update

--- matJS.py
+++ matJS.py
@@ -2,7 +2,7 @@
 # @Author: UnsignedByte
 # @Date:   18:37:12, 28-Jan-2020
 # @Last Modified by:   UnsignedByte
-# @Last Modified time: 17:27:29, 12-Feb-2020
+# @Last Modified time: 15:51:49, 15-Feb-2020
 
 import json
 import os.path
@@ -22,12 +22,12 @@ with open(os.path.join(root, 'default.html')) as f:
 with open(os.path.join(root, 'jquery-3.4.1.min.js')) as f:
 	jquery = f.read()
 
-with open(os.path.join(root, 'jquery-ui.min.js')) as f:
-	jqueryui = f.read()
+# with open(os.path.join(root, 'jquery-ui.min.js')) as f:
+# 	jqueryui = f.read()
 
-with open(os.path.join(root, 'jquery-ui.min.css')) as f:
-	jqueryui = f.read()
+# with open(os.path.join(root, 'jquery-ui.min.css')) as f:
+# 	jqueryui = f.read()
 
 with open(os.path.join(root, 'Results', name, f'{name}.html'), 'w') as f:
-	f.write(default.replace('$$datainput$$', data).replace("$$JQUERY$$", jquery).replace("$$JQUERYUI$$", jqueryui))
+	f.write(default.replace('$$datainput$$', data).replace("$$JQUERY$$", jquery))
 	f.close()
