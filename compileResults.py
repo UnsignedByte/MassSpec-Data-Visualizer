c26d091bed6813a74b5c8ba13499854ef4b5a7e8
UnsignedByte <edlam1889@gmail.com>
2020-06-09 16:25:11 -0700

    add optional all folder compilation as well as colored error text

--- compileResults.py
+++ compileResults.py
@@ -2,44 +2,39 @@
 # @Author: UnsignedByte
 # @Date:   18:37:12, 28-Jan-2020
 # @Last Modified by:   UnsignedByte
-# @Last Modified time: 14:55:31, 09-Jun-2020
+# @Last Modified time: 16:23:27, 09-Jun-2020
 
 import csv
 import json
 import base64
 import os.path
+import traceback
+
+# Colored terminal text for python
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 root = os.path.abspath(os.path.dirname(__file__)) # Root directory
 
-
-name = input("Result Folder Name: ") # Get file to read
-
-resultsFolder = os.path.join(root, 'Results', name)
+#read in default html page
 
 with open(os.path.join(root, 'webdefault', 'default.html')) as f:
 	default = f.read()
 
-data = {};
+with open(os.path.join(root, 'webdefault', 'default.js')) as f:
+	default = default.replace('$$DEFAULTJS$$', f.read());
 
-# fileIDs
+with open(os.path.join(root, 'webdefault', 'default.css')) as f:
+	default = default.replace('$$DEFAULTCSS$$', f.read());
 
-with open(os.path.join(resultsFolder, 'fileIDs.csv')) as f:
-	reader = csv.DictReader(f)
-	data['ids'] = []
-	for row in reader:
-		data['ids'].append(row)
-
-# get all raws
-rawsdir = os.fsencode(os.path.join(resultsFolder, 'Raws'))
-
-#loop through raws and put in data
-for file in os.listdir(rawsdir):
-	filename = os.fsdecode(file)
-	if filename.endswith(".json"):
-		with open(os.path.join(resultsFolder, 'Raws', filename)) as f:
-			data = {**data, **json.load(f)};
-
-# Read in js tools
+# Read in js/css tools
 
 with open(os.path.join(root, 'webtools', 'jquery-3.4.1.min.js')) as f:
 	default = default.replace("$$JQUERY$$", f.read())
@@ -68,20 +63,57 @@ with open(os.path.join(root, 'webtools', 'clusterize.min.js')) as f:
 with open(os.path.join(root, 'webtools', 'clusterize.css')) as f:
 	default = default.replace("$$CLUSTERIZECSS$$", f.read())
 
-with open(os.path.join(root, 'webdefault', 'default.js')) as f:
-	default = default.replace('$$DEFAULTJS$$', f.read());
-	
-with open(os.path.join(root, 'webdefault', 'default.css')) as f:
-	default = default.replace('$$DEFAULTCSS$$', f.read());
-
 # with open(os.path.join(root, 'jquery-ui.min.js')) as f:
 # 	jqueryui = f.read()
 
 # with open(os.path.join(root, 'jquery-ui.min.css')) as f:
 # 	jqueryui = f.read()
 
-default = default.replace('$$datainput$$', json.dumps(data)) # Place data into html file
+name = input("Result Folder Name: ") # Get file to read
 
-with open(os.path.join(resultsFolder, f'{name}.html'), 'w') as f:
-	f.write(default)
-	f.close()
+def insertData(name):
+	resultsFolder = os.path.join(root, 'Results', name)
+
+	data = {};
+
+	# fileIDs
+
+	with open(os.path.join(resultsFolder, 'fileIDs.csv')) as f:
+		reader = csv.DictReader(f)
+		data['ids'] = []
+		for row in reader:
+			data['ids'].append(row)
+
+	# get all raws
+	rawsdir = os.fsencode(os.path.join(resultsFolder, 'Raws'))
+
+	#loop through raws and put in data
+	for file in os.listdir(rawsdir):
+		filename = os.fsdecode(file)
+		if filename.endswith(".json"):
+			with open(os.path.join(resultsFolder, 'Raws', filename)) as f:
+				data = {**data, **json.load(f)};
+
+	return default.replace('$$datainput$$', json.dumps(data)) # Place data into html file
+
+def saveData(x):
+	print(f"{bcolors.HEADER}Compiling files for {x}.{bcolors.ENDC}")
+	with open(os.path.join(os.path.join(root, 'Results', x), f'{x}.html'), 'w') as f:
+		try:
+			f.write(insertData(x))
+			print(f"{bcolors.OKGREEN}Compilation completed successfully.{bcolors.ENDC}")
+		except Exception as e:
+			print(f"{bcolors.FAIL}Compilation error occured. Results data may not be formatted properly.\nFull error:\n{traceback.format_exc()}{bcolors.ENDC}")
+		finally:
+			f.close()
+
+if not name: # name is empty
+	print(f"{bcolors.WARNING}No dataset supplied. Compiling all datasets by default.{bcolors.ENDC}")
+	for x in os.listdir(os.path.join(root, 'Results')):
+		if os.path.isdir(os.path.join(root, 'Results',x)):
+			saveData(x);
+else:
+	with open(os.path.join(os.path.join(root, 'Results', name), f'{name}.html'), 'w') as f:
+			saveData(name);
+
+
