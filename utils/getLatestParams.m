b441e098cbf33a77c30b40a49dcb5d3ae21034dd
UnsignedByte <edlam1889@gmail.com>
2020-07-11 13:59:53 -0700

    return HASHMAP

--- utils/getLatestParams.m
+++ utils/getLatestParams.m
@@ -4,12 +4,12 @@ function params = getLatestParams(p)
 
 	list = dir([p, '/*.txt']);
 	% cell array of the times
-	parsedTokens = regexp({list.name}, '(.+)_(\d\d-\d\d-\d{4}_\d\d:\d\d:\d\d)\.txt', 'tokens'); % list of parsed tokens
+	parsedTokens = regexp({list.name}, '(.+)_((?:\d\d-){2}\d{4}_(?:\d\d:){2}\d\d)\.txt', 'tokens'); % list of parsed tokens
 	parsedTokens = horzcat(parsedTokens{:}); % unnest inner arrays
 	parsedTokens = vertcat(parsedTokens{:}); % list of 1x2 cells to 10x2 matrix
 	parsedTokens(:,2) = num2cell(cellfun(@(x) datetime(x, 'InputFormat', fmt), parsedTokens(:,2))); % convert objects to datetime
 
-	% get max datetime (latest) for each type of param file
-	params = cellfun(@(x) {x, [x '_' datestr(max([parsedTokens{contains(parsedTokens(:,1), x),2}]), fmt) '.txt']}, unique(parsedTokens(:,1)), 'UniformOutput', false);
-	params = vertcat(params{:}); % unnest cells again
+	% return map containing filename for the latest version of each param
+	keyset = unique(parsedTokens(:,1));
+	params = containers.Map(keyset, cellfun(@(x) [x '_' datestr(max([parsedTokens{contains(parsedTokens(:,1), x),2}]), fmt) '.txt'], keyset, 'UniformOutput', false));
 end
