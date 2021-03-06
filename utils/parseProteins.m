a80bf184371942769b4f8f834b90d92c9b7a285b
UnsignedByte <edlam1889@gmail.com>
2021-03-06 15:08:51 -0800

    fix

--- utils/parseProteins.m
+++ utils/parseProteins.m
@@ -1,16 +1,18 @@
 function parsed = parseProteins(proteins) %return info about each protein in a list of protein names
     regStr = '^>?(.+?)\|(.+?)\|(.+?)_(.+?)\s((?:.+?)+?)(\sOS=(?:.|\s|)+?)(\sOX=\d+?)(\sGN=.+?)?(\sPE=\d+)?(\sSV=\d+)?$';
 
+    %disp(proteins)
+    
     function p = parse(x)
     	if regexp(x, regStr)
     		p = regexp(x, regStr, 'tokens');
-    		p{1}(1,6:10) = cellfun(@(x) split(x, '='), p{1}(1, 6:10), 'UniformOutput', false); %Split by = sign
+    		p{1}(1,6:10) = cellfun(@(x) split([x '='], '='), p{1}(1, 6:10), 'UniformOutput', false); %Split by = sign
     		p{1}(1,6:10) = cellfun(@(x) x{2}, p{1}(1, 6:10), 'UniformOutput', false); %Take value only
     	else
     		p = cell(1,10);
     		p{3} = regexp(x, '^>?(.+?)$', 'tokens');
     		p{3} = p{3}{1}{1};
-    		p = {p}
+    		p = {p};
     	end
     end
 
